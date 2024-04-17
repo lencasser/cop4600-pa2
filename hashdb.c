4b69b8257a83c7dea7009f31842ef662c0434f28
Bennett <coaster@fridayfilmday.com>
2024-04-17 18:32:37 -0400

    added input file commands.txt and text within hashdb.h

--- hashdb.c
+++ hashdb.c
@@ -1,1 +1,10 @@
-// hashdb.c file
+// hashdb.c file - 	Your main program that reads the commands.txt and produces output to the console
+
+
+
+
+int main (void) {
+
+
+    return 0;
+}
--- hashdb.h
+++ hashdb.h
@@ -1,1 +1,8 @@
 // hashdb.h file
+typedef struct hash_struct
+{
+  int hash; //originally "uint32_t hash;" -BS
+  char name[50];
+  int salary; //originally "uint32_t salary;" -BS
+  struct hash_struct *next;
+} hashRecord;
