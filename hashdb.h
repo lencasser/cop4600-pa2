dfa5626882246e4c4760c54a8b1320ab1e40b44d
srra <160298789+stannie64@users.noreply.github.com>
2024-04-18 19:08:11 -0400

    hashdb.h w/ functions + list struct
    added list struct because there should be a lock inside of a list struct from my understanding. like so that there can be a lock for the list. yeah << copy pasted from hashdb.c's description but NOTE: the lock in list thing doesn't work bc of an incompatible pointer type error, C thinks rwlock_t is an int and i'm forgetting how to fix that even though i know i have before UGH so sorry

--- hashdb.h
+++ hashdb.h
@@ -1,8 +1,22 @@
-// hashdb.h file
-typedef struct hash_struct
-{
-  int hash; //originally "uint32_t hash;" -BS
-  char name[50];
-  int salary; //originally "uint32_t salary;" -BS
-  struct hash_struct *next;
+#include <stdint.h>
+
+typedef struct hash_struct {
+    uint32_t hash;
+    char name[50];
+    uint32_t salary;
+    struct hash_struct *next;
 } hashRecord;
+
+typedef struct list {
+    hashRecord *head;
+    rwlock_t *lock; // this doesn't work i'm prob doing smth dumb
+} list;
+
+// depending on how the lock stuff works might add a list struct?
+
+extern uint32_t jenkins_hash(char *key, size_t length);
+extern list * create_list();
+extern hashRecord * create_record(char *key, uint32_t value, uint32_t hash);
+extern void insert(char *key, int value, hashRecord *head);
+extern void delete(char *key);
+extern uint32_t search(char *key, hashRecord *head);
