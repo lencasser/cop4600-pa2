135dd51d0c02e1293401b41c1f72bcf23e9f76a1
emoteshark <102431700+emoteshark@users.noreply.github.com>
2024-04-18 02:51:11 -0400

    implemented locks

--- rwlocks.c
+++ rwlocks.c
@@ -1,93 +1,35 @@
-// rwlocks.c
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-
-#include "common.h"
-#include "common_threads.h"
-
-#ifdef linux
-#include <semaphore.h>
-#elif __APPLE__
-#include "zemaphore.h"
-#endif
-
+#include "rwlocks.h"
 
 void rwlock_init(rwlock_t *lock) {
     lock->readers = 0;
-    Sem_init(&lock->lock, 1); 
-    Sem_init(&lock->writelock, 1); 
+    sem_init(&lock->lock, 0, 1);
+    sem_init(&lock->writelock, 0, 1);
 }
 
 void rwlock_acquire_readlock(rwlock_t *lock) {
-    Sem_wait(&lock->lock);
+    sem_wait(&lock->lock);
     lock->readers++;
     if (lock->readers == 1)
-	Sem_wait(&lock->writelock);
-    Sem_post(&lock->lock);
+	sem_wait(&lock->writelock);
+    sem_post(&lock->lock);
+    printf("READ LOCK ACQUIRED");
 }
 
 void rwlock_release_readlock(rwlock_t *lock) {
-    Sem_wait(&lock->lock);
+    sem_wait(&lock->lock);
     lock->readers--;
     if (lock->readers == 0)
-	Sem_post(&lock->writelock);
-    Sem_post(&lock->lock);
+	sem_post(&lock->writelock);
+    sem_post(&lock->lock);
+    printf("READ LOCK RELEASED");
 }
 
 void rwlock_acquire_writelock(rwlock_t *lock) {
-    Sem_wait(&lock->writelock);
+    sem_wait(&lock->writelock);
+    printf("WRITE LOCK ACQUIRED");
 }
 
 void rwlock_release_writelock(rwlock_t *lock) {
-    Sem_post(&lock->writelock);
-}
-
-int read_loops;
-int write_loops;
-int counter = 0;
-
-rwlock_t mutex;
-
-void *reader(void *arg) {
-    int i;
-    int local = 0;
-    for (i = 0; i < read_loops; i++) {
-	rwlock_acquire_readlock(&mutex);
-	local = counter;
-	rwlock_release_readlock(&mutex);
-	printf("read %d\n", local);
-    }
-    printf("read done: %d\n", local);
-    return NULL;
-}
-
-void *writer(void *arg) {
-    int i;
-    for (i = 0; i < write_loops; i++) {
-	rwlock_acquire_writelock(&mutex);
-	counter++;
-	rwlock_release_writelock(&mutex);
-    }
-    printf("write done\n");
-    return NULL;
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-	fprintf(stderr, "usage: rwlock readloops writeloops\n");
-	exit(1);
-    }
-    read_loops = atoi(argv[1]);
-    write_loops = atoi(argv[2]);
-    
-    rwlock_init(&mutex); 
-    pthread_t c1, c2;
-    Pthread_create(&c1, NULL, reader, NULL);
-    Pthread_create(&c2, NULL, writer, NULL);
-    Pthread_join(c1, NULL);
-    Pthread_join(c2, NULL);
-    printf("all done\n");
-    return 0;
+    sem_post(&lock->writelock);
+    printf("WRITE LOCK RELEASED");
 }
--- rwlocks.h
+++ rwlocks.h
@@ -1,20 +1,24 @@
-// rwlocks.h file
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-
-#include "common.h"
-#include "common_threads.h"
-
-#ifdef linux
 #include <semaphore.h>
-#elif __APPLE__
-#include "zemaphore.h"
-#endif
 
 typedef struct _rwlock_t {
     sem_t writelock;
     sem_t lock;
     int readers;
 } rwlock_t;
+
+//creats a lock
+void rwlock_init(rwlock_t *lock);
+
+
+//read locks do not block other read locks. Multiple readers can work at the same time
+//read locks only block writers. Writer locks block everyone
+
+//acquires the lock for a reader
+void rwlock_acquire_readlock(rwlock_t *lock);
+//releases the lock for a reader
+void rwlock_release_readlock(rwlock_t *lock);
+
+//acquires the lock for a writer
+void rwlock_acquire_writelock(rwlock_t *lock);
+//releases the lock for a writer
+void rwlock_release_writelock(rwlock_t *lock);
