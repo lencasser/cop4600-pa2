d5eb4ec98d742473438294628a40602fa75f74d3
srra <160298789+stannie64@users.noreply.github.com>
2024-04-19 15:34:03 -0400

    chash.c edit srra
    - modified functions to be same as hashdb.c 's structure for them, depending on implementation may change back who knows
    
    - fixed some errors
    
    - added creation a hash table in main
    
    correctness to be determined...

--- chash.c
+++ chash.c
@@ -1,11 +1,10 @@
 // chash.c file
-#include <stdio.h>;
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>;
-
-
-#include "hashdb.h";
-#include "hashdb.c";
+#include <string.h>
+#include <pthread.h>
+#include "hashdb.h"
+#include "hashdb.c"
 
 void print() {
     
@@ -17,16 +16,22 @@ void finalPrint() {
     print();
 }
 
-
-
-void parseCommand(char* currCommand, char* currParameter1, char* currParameter2) {
+// so sorry if adding the list *table ptrs to everything is wrong
+void parseCommand(char* currCommand, char* currParameter1, char* currParameter2, list *table) {
     print("%s", currCommand);
     if (strcmp(currCommand,"threads")) {
         return;
     }
     else if (strcmp(currCommand,"insert")) {
         int value = atoi(currParameter1);
-        insert(currParameter1,currParameter2,"TODO: Put hash here");
+        
+        // srra note: i know the TODO said add hash, but you don't
+        // actually need the hash as a parameter since aedo said
+        // the insert function computes the hash first
+
+        // the passing in of table and stuff might change depending on
+        // how the lock stuff is set up (?) and like. um. idk   . ah
+        insert(currParameter1, atoi(currParameter2), table);
     }
     else if (strcmp(currCommand,"print")) {
         //TODO: Get read lock
@@ -34,10 +39,13 @@ void parseCommand(char* currCommand, char* currParameter1, char* currParameter2)
         //TODO: Release read lock
     }
     else if (strcmp(currCommand,"search")) {
-        search(currParameter1,"TODO: Put hash here");
+        // again i might be missing something huge here and maybe
+        // we can remove table as a parameter after all. for now,
+        // edited in accordance with hashdb.c function structure
+        search(currParameter1, table);
     }
     else if (strcmp(currCommand,"delete")) {
-        delete(currParameter1);
+        delete(currParameter1, table);
     }
     else {
         printf("Command %s not found", currCommand);
@@ -45,9 +53,6 @@ void parseCommand(char* currCommand, char* currParameter1, char* currParameter2)
 
     return;
 }
-
-
-
 
 int main (void) {
 
@@ -63,6 +68,9 @@ int main (void) {
     char currParameter1[100];
     char currParameter2[200];
 
+    // creating hash table in advance wee
+    list *table = create_list();
+
     // Read the content and print it
     currChar = getchar();
     while(currChar != EOF) { //If we're not at the end of the file yet
@@ -75,7 +83,7 @@ int main (void) {
                 currChar = getchar();
             }
 
-            currChar = getChar(); //Move currChar from ',' to the next letter
+            currChar = getchar(); //Move currChar from ',' to the next letter
             currentWordIdx = 0; //Reset the index
 
             while (currChar != ',') { //Get the first parameter
@@ -84,7 +92,7 @@ int main (void) {
                 currChar = getchar();
             }
 
-            currChar = getChar(); //Move currChar from ',' to the next letter
+            currChar = getchar(); //Move currChar from ',' to the next letter
             currentWordIdx = 0; //Reset the index
 
             while (currChar != ',') { //Get the second parameter
@@ -93,7 +101,7 @@ int main (void) {
                 currChar = getchar();
             }
 
-            parseCommand(currCommand,currParameter1,currParameter2);
+            parseCommand(currCommand,currParameter1,currParameter2, table);
             
         }
         //printf("%s", inputString);
